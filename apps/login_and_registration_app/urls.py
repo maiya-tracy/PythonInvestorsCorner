95b77b5b47de5c55fb661360b64c4397c4a471b5
Maiya Tracy <maiya.tracy@gmail.com>
2019-06-26 12:05:58 -0700

    added github connection

--- apps/login_and_registration_app/urls.py
+++ apps/login_and_registration_app/urls.py
@@ -1,19 +1,20 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home),
+    url(r'^$', views.home, name='landing'),
     url(r'^registration$', views.registration),
     url(r'^registration/processing$', views.registration_process),
-    url(r'^login$', views.login),
+    url(r'^login$', views.login, name='login'),
     url(r'^login/processing$', views.login_process),
-    url(r'^news$', views.news),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
+    url(r'^news$', views.news, name='news'),
     url(r'^investments$', views.investments),
     url(r'^investments/processing', views.investments_process),
     url(r'^chatroom/(?P<chatroomid>\d+)$', views.view_chatroom),
     url(r'^token', views.token, name='token'),
     url(r'^communities$', views.community),
-    url(r'^logout$', views.logout),
+    url(r'^logout$', views.logout, name='logout'),
     # url(r'^user/chatroom/add', views.add_chatroom),
     # url(r'^user/chatroom/add/processing$' views.add_chatroom_process),
 ]
